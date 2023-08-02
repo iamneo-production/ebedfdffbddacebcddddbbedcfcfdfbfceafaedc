9ea381ab8e87da7f6e30b47647e5243f77558888
Iamneo <techadmin@iamneo.ai>
2023-08-02 10:22:00 +0000

    Backup Commit

--- Task-Management/Task-Management/src/main/java/com/example/Task/Management/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Task.Management;
-
-import com.example.Task.Management.model.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task,Long> {
-}
